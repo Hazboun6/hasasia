406b20102b1a8822e76743c85fc4b2c3c035fb75
Hazboun6 <jeffrey.hazboun@gmail.com>
2019-04-02 19:46:21 -0700

    Added HD correlations funcn. Added Sensitivity Curve class.

--- pta_sensitivity/sensitivity.py
+++ pta_sensitivity/sensitivity.py
@@ -2,10 +2,11 @@
 from __future__ import print_function
 """Main module."""
 import numpy as np
+import itertools as it
 
 ## Some constants
-yr = 365.25*24*3600
-fyr = 1/yr
+yr_sec = 365.25*24*3600
+fyr = 1/yr_sec
 
 def R_matrix(designmatrix, N):
     """
@@ -51,8 +52,8 @@ def G_matrix(designmatrix):
 
     return U[:,m:]
 
-def transmission(designmatrix, toas, N,
-                 nf=200, fmin=None, fmax=2e-7,
+def transmission(designmatrix, toas, N=None,
+                 nf=200, fmin=None, fmax=2e-7, freqs=None,
                  exact_astro_freqs = False, from_G=False):
     """
     Calculate the transmission function for a given pulsar design matrix, TOAs
@@ -87,6 +88,10 @@ def transmission(designmatrix, toas, N,
         Whether to use G matrix for transmission function calculate. Default is
         False, in which case R matrix is used.
     """
+    if not from_G and N is None:
+        err_msg = 'Covariance Matrix must be provided if constructing'
+        err_msg += ' from R-matrix.'
+        raise ValueError(err_msg)
 
     M = designmatrix
 
@@ -97,19 +102,23 @@ def transmission(designmatrix, toas, N,
 
     # make filter
     f0 = 1 / T
-    if fmin is None:
-        fmin = f0/5
-    ff = np.logspace(np.log10(fmin), np.log10(fmax), nf,dtype='float64')
-    if exact_astro_freqs:
-        ff = np.sort(np.append(ff,[fyr,2*fyr]))
-        nf +=2
+    if freqs is None:
+        if fmin is None:
+            fmin = f0/5
+        ff = np.logspace(np.log10(fmin), np.log10(fmax), nf,dtype='float64')
+        if exact_astro_freqs:
+            ff = np.sort(np.append(ff,[fyr,2*fyr]))
+            nf +=2
+    else:
+        ff = freqs
 
     Tmat = np.zeros(nf, dtype='float64')
     if from_G:
         G = G_matrix(M)
         m = G.shape[1]
         for ct, f in enumerate(ff):
-            Tmat[ct] = np.real(np.sum(np.exp(-1j*2*np.pi*f*tm)*np.matmul(G,G.T))/m)
+            Tmat[ct] = np.real(np.sum(np.exp(-1j*2*np.pi*f*tm)
+                                      *np.matmul(G,G.T))/m)
     else:
         R = R_matrix(M, N)
         N_TOA = M.shape[0]
@@ -122,3 +131,125 @@ def transmission(designmatrix, toas, N,
 def response(freqs):
     """Timing residual response function."""
     return 1/(12*np.pi*freqs**2)
+
+class sensitivity_curve(object):
+    """
+    Class for constructing PTA sensitivity curves.
+    """
+    def __init__(self, nf, Tspan, fmin=None, fmax=1e-7):
+        self.nf = nf
+        self.Tspan = Tspan*yr_sec
+        f0 = 1 / Tspan
+        if fmin is None:
+            fmin = f0/5
+
+        self.freqs = np.logspace(np.log10(fmin),np.log10(fmax),nf)
+
+    def S_n(self):
+        """Strain power sensitivity. """
+        return None
+
+    def h_c(self):
+        """Characteristic strain sensitivity"""
+        return None
+
+    def Omega_gw(self):
+        """Energy Density sensitivity"""
+        return None
+
+    def psd_postfit(self):
+        """Postfit Residual Power"""
+        return None
+
+    def psd_prefit(self):
+        """Prefit Residual Power"""
+        return None
+
+    def set_psr_sky_location(self,lat,long):
+        """Set Latitude and Longitude of pulsars"""
+        self._lat = lat
+        self._long = long
+
+    def calc_trans():
+        """Calculate transmission functions for pulsars."""
+
+    def calc_power():
+        """Calculate """
+
+    def create_white_noise_pow(self, sigma=None, dt=None, WN=None):
+        if WN is None and np.logical_and(sigma is not None, dt is not None):
+            self._white_noise = 2.0 * dt * (sigma)**2
+            self._white_noise *= np.ones_like(self.freqs)
+        elif WN is not None and np.logical_and(sigma is None, dt is None):
+            self._white_noise = WN
+
+        return self._white_noise
+
+    def create_red_noise_pow(self, A=None, gamma=None, RN=None):
+        if RN is None and np.logical_and(A is not None, gamma is not None):
+            ff = self.freqs
+            self._red_noise = A**2*(ff/fyr)**(-gamma)/(12*np.pi**2)
+            self._red_noise *= yr_sec**3
+
+        elif RN is not None and np.logical_and(A is None, gamma is None):
+            self._red_noise = RN
+
+        return self._red_noise
+
+    def create_pulsar_term_noise_pow(self, Agw=None, gamma=None, PTN=None):
+        if PTN is None and np.logical_and(Agw is not None, gamma is not None):
+            ff = self.freqs
+            self._pulsar_term_noise = Agw**2*(ff/fyr)**(-gamma)/(12*np.pi**2)
+            self._pulsar_term_noise *= yr_sec**3
+
+        elif PTN is not None and np.logical_and(Agw is None, gamma is None):
+            self._pulsar_term_noise = PTN
+
+        return self._pulsar_term_noise
+
+def HellingsDownsCoeff(phi, theta):
+    """
+    Calculate Hellings and Downs coefficients from two lists of sky positions.
+
+    Parameters
+    ----------
+
+    phi : array, list
+        Pulsar axial coordinate.
+
+    theta: array, list
+        Pulsar azimuthal coordinate.
+
+    Returns
+    -------
+
+    ThetaIJ : array
+        Array of angles between pairs of pulsars.
+
+    alphaIJ : array
+        Array of Hellings and Downs relation coefficients.
+
+    pairs: array
+        A 2xNpair array of pair indices corresponding to input order of sky
+        coordinates.
+
+    alphaRSS : float
+        Root-sum-squared value of all Hellings-Downs coefficients.
+
+    """
+
+    Npsrs = len(phi)
+    # Npairs = np.int(Npsrs * (Npsrs-1) / 2.)
+    psr_idx = np.arange(Npsrs)
+    pairs = list(it.combinations(psr_idx,2))
+    first, second = list(map(list, zip(*pairs)))
+    cosThetaIJ = np.cos(theta[first]) * np.cos(theta[second]) \
+                    + np.sin(theta[first]) * np.sin(theta[second]) \
+                    * np.cos(phi[first] - phi[second])
+    X = (1. - cosThetaIJ) / 2.
+    alphaIJ = [1.5*x*np.log(x) - 0.25*x/4. + 0.5 if x!=0 else 1. for x in X]
+    alphaIJ = np.array(alphaIJ)
+
+    # calculate rss (root-sum-squared) of hellings-downs factor
+    alphaRSS = np.sqrt(np.sum(alphaIJ**2))
+    return np.arccos(cosThetaIJ), alphaIJ, np.array([first,second]), alphaRSS
