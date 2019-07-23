3c95283ab6bbe2ea922a5e2dfb32afc4a9855d18
Hazboun6 <jeffrey.hazboun@gmail.com>
2019-07-23 16:17:16 -0700

    New tests.

--- tests/test_hasasia.py
+++ tests/test_hasasia.py
@@ -4,24 +4,45 @@
 """Tests for `hasasia` package."""
 
 import pytest
+import numpy as np
 
-
-from hasasia import sensitivity
+from hasasia import sensitivity as sens
 from hasasia import skymap
-from hasasia import sim
+from hasasia.sim import sim_pta
 
+phi = np.random.uniform(0, 2*np.pi,size=34)
+theta = np.random.uniform(0, np.pi,size=34)
+freqs = np.logspace(np.log10(5e-10),np.log10(5e-7),400)
+timespans=[11.4 for ii in range(10)]
+timespans.extend([3.0 for ii in range(24)])
+
+phi_gw = np.random.uniform(0, 2*np.pi,size=2048)
+theta_gw = np.random.uniform(0, np.pi,size=2048)
 
 @pytest.fixture
-def response():
-    """Sample pytest fixture.
-
-    See more at: http://doc.pytest.org/en/latest/fixture.html
+def pta_simple():
     """
-    # import requests
-    # return requests.get('https://github.com/audreyr/cookiecutter-pypackage')
+    Sample set of pulsars for testing. All Tspan=11.4 yrs
+    """
+    return sim_pta(timespan=11.4, cad=23, sigma=1e-7,
+                   phi=phi, theta=theta, Npsrs=34)
 
+@pytest.fixture
+def pta_heter():
+    """
+    Sample set of pulsars for testing.
+    """
+    return sim_pta(timespan=timespans,cad=23,sigma=1e-7,
+                   phi=phi,theta=theta)
 
-def test_content(response):
-    """Sample pytest test function with the pytest fixture as an argument."""
-    # from bs4 import BeautifulSoup
-    # assert 'GitHub' in BeautifulSoup(response.content).title.string
+def test_simple_pta(pta_simple):
+    """Sensitivity Tests"""
+    spectra = []
+    for p in pta_simple:
+        sp = sens.Spectrum(p, freqs=freqs)
+        sp.Tf
+        spectra.append(sp)
+
+    sens.GWBSensitivityCurve(spectra)
+    sens.DeterSensitivityCurve(spectra)
+    skymap.SkySensitivity(spectra,theta_gw,phi_gw)
