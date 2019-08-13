bb738fcdb50b01f6a0abc0d99b32d83166bc460d
Hazboun6 <jeffrey.hazboun@gmail.com>
2019-08-13 14:19:56 -0700

    Upped version again after fixing image for PyPI

--- setup.py
+++ setup.py
@@ -45,6 +45,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/Hazboun6/hasasia',
-    version='0.1.3',
+    version='0.1.4',
     zip_safe=False,
 )
