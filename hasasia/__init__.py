0ca8c428cba22bbcb05fe1ab9a56fd0acc5ca477
Hazboun6 <jeffrey.hazboun@gmail.com>
2019-08-29 22:24:49 -0700

    Upping version.

--- hasasia/__init__.py
+++ hasasia/__init__.py
@@ -4,7 +4,7 @@
 
 __author__ = """Jeffrey S. Hazboun"""
 __email__ = 'jeffrey.hazboun@gmail.com'
-__version__ = '0.1.5'
+__version__ = '0.1.6'
 
 citation = 'Hazboun, J.S., Romano, J.R., Smith, T.L. '
 citation += 'Realistic sensitivity curves for pulsar timing arrays. '
