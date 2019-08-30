8b6566c69548e371b66a40bbaad2f2f1f4f097aa
Hazboun6 <jeffrey.hazboun@gmail.com>
2019-08-30 10:58:09 -0700

    Upped version number.

--- setup.py
+++ setup.py
@@ -45,6 +45,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/Hazboun6/hasasia',
-    version='0.1.6',
+    version='0.1.7',
     zip_safe=False,
 )
