3b6de5722b6ce563874e0b0f4154789e26ad5c4d
Hazboun6 <jeffrey.hazboun@gmail.com>
2019-09-20 12:22:32 -0700

    Changed to 1.0.0 version for JOSS submission.

--- setup.py
+++ setup.py
@@ -45,6 +45,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/Hazboun6/hasasia',
-    version='0.1.7',
+    version='1.0.0',
     zip_safe=False,
 )
