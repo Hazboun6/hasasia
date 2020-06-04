ea1420b85d83630b82a2b4901ea551b737daa2ea
Hazboun6 <jeffrey.hazboun@gmail.com>
2020-06-04 14:18:28 -0700

    Added tests for new functionality

--- tests/test_skymap.py
+++ tests/test_skymap.py
@@ -9,48 +9,117 @@ import hasasia.sensitivity as hsen
 import hasasia.sim as hsim
 import hasasia.skymap as hsky
 
+@pytest.fixture
+def sm_simple():
+    '''Test and keep a simple sensitivity skymap'''
+    #Make a set of random sky positions
+    phi = np.random.uniform(0, 2*np.pi,size=33)
+    cos_theta = np.random.uniform(-1,1,size=33)
+    theta = np.arccos(cos_theta)
 
-#Make a set of random sky positions
-phi = np.random.uniform(0, 2*np.pi,size=33)
-cos_theta = np.random.uniform(-1,1,size=33)
-theta = np.arccos(cos_theta)
+    #Adding one well-placed sky position for plots.
+    phi = np.append(np.array(np.deg2rad(60)),phi)
+    theta = np.append(np.array(np.deg2rad(50)),theta)
 
-#Adding one well-placed sky position for plots.
-phi = np.append(np.array(np.deg2rad(60)),phi)
-theta = np.append(np.array(np.deg2rad(50)),theta)
-
-#Define the timsespans and TOA errors for the pulsars
-timespans = np.random.uniform(3.0,11.4,size=34)
-Tspan = timespans.max()*365.25*24*3600
-sigma = 1e-7 # 100 ns
-
-
-#Simulate a set of identical pulsars, with different sky positions.
-psrs = hsim.sim_pta(timespan=11.4, cad=23, sigma=sigma,
-                    phi=phi, theta=theta)
+    #Define the timsespans and TOA errors for the pulsars
+    timespans = np.random.uniform(3.0,11.4,size=34)
+    Tspan = timespans.max()*365.25*24*3600
+    sigma = 1e-7 # 100 ns
 
 
-
-freqs = np.logspace(np.log10(1/(5*Tspan)),np.log10(2e-7),500)
-spectra = []
-for p in psrs:
-    sp = hsen.Spectrum(p, freqs=freqs)
-    sp.NcalInv
-    spectra.append(sp)
+    #Simulate a set of identical pulsars, with different sky positions.
+    psrs = hsim.sim_pta(timespan=11.4, cad=23, sigma=sigma,
+                        phi=phi, theta=theta)
 
 
-#Normally use the healpy functions to get the sky coordinates
-#Here just pull random coordinates using numpy to avoid needing healpy
-phi_gw = np.random.uniform(0, 2*np.pi,size=1000)
-cos_theta_gw = np.random.uniform(-1,1,size=1000)
-theta_gw = np.arccos(cos_theta_gw)
 
-SM=hsky.SkySensitivity(spectra,theta_gw, phi_gw)
-hCirc = hsky.h0_circ(1e9,200,5e-9).to('')
+    freqs = np.logspace(np.log10(1/(5*Tspan)),np.log10(2e-7),500)
+    spectra = []
+    for p in psrs:
+        sp = hsen.Spectrum(p, freqs=freqs)
+        sp.NcalInv
+        spectra.append(sp)
 
-SNR = SM.SNR(hCirc.value)
 
-h_divA = (hsky.h_circ(1e9,200,5e-9,Tspan,SM.freqs)
-          /hsky.h0_circ(1e9,200,5e-9)).value
+    #Normally use the healpy functions to get the sky coordinates
+    #Here just pull random coordinates using numpy to avoid needing healpy
+    phi_gw = np.random.uniform(0, 2*np.pi,size=1000)
+    cos_theta_gw = np.random.uniform(-1,1,size=1000)
+    theta_gw = np.arccos(cos_theta_gw)
 
-Amp = SM.A_gwb(h_divA)
+    SM = hsky.SkySensitivity(spectra,theta_gw, phi_gw)
+
+    return SM
+
+@pytest.fixture
+def spectra_theta_phi():
+    '''Test and keep a simple sensitivity skymap'''
+    #Make a set of random sky positions
+    phi = np.random.uniform(0, 2*np.pi,size=33)
+    cos_theta = np.random.uniform(-1,1,size=33)
+    theta = np.arccos(cos_theta)
+
+    #Adding one well-placed sky position for plots.
+    phi = np.append(np.array(np.deg2rad(60)),phi)
+    theta = np.append(np.array(np.deg2rad(50)),theta)
+
+    #Define the timsespans and TOA errors for the pulsars
+    timespans = np.random.uniform(3.0,11.4,size=34)
+    Tspan = timespans.max()*365.25*24*3600
+    sigma = 1e-7 # 100 ns
+
+
+    #Simulate a set of identical pulsars, with different sky positions.
+    psrs = hsim.sim_pta(timespan=11.4, cad=23, sigma=sigma,
+                        phi=phi, theta=theta)
+
+    freqs = np.logspace(np.log10(1/(5*Tspan)),np.log10(2e-7),500)
+    spectra = []
+    for p in psrs:
+        sp = hsen.Spectrum(p, freqs=freqs)
+        sp.NcalInv
+        spectra.append(sp)
+
+
+    #Normally use the healpy functions to get the sky coordinates
+    #Here just pull random coordinates using numpy to avoid needing healpy
+    phi_gw = np.random.uniform(0, 2*np.pi,size=1000)
+    cos_theta_gw = np.random.uniform(-1,1,size=1000)
+    theta_gw = np.arccos(cos_theta_gw)
+
+    return spectra, theta_gw, phi_gw
+
+def test_skymap(sm_simple):
+    '''test sky map functionality.'''
+
+    hCirc = hsky.h0_circ(1e9,200,5e-9).to('')
+
+    sm_simple.SNR(hCirc.value)
+
+    sm_simple.h_thresh(2)
+
+    h_divA = (hsky.h_circ(1e9,200,5e-9,sm_simple.Tspan,sm_simple.freqs)
+              /hsky.h0_circ(1e9,200,5e-9)).value
+
+    Amp = sm_simple.A_gwb(h_divA)
+
+def test_pulsar_term_skymap(spectra_theta_phi):
+    '''scalar test'''
+    spectra, theta_gw, phi_gw = spectra_theta_phi
+    SM = hsky.SkySensitivity(spectra, theta_gw, phi_gw, pulsar_term=True)
+
+
+def test_scalar_long_skymap(spectra_theta_phi):
+    '''scalar test'''
+    spectra, theta_gw, phi_gw = spectra_theta_phi
+    SM = hsky.SkySensitivity(spectra, theta_gw, phi_gw, pol='scalar-long')
+
+def test_vector_long_skymap(spectra_theta_phi):
+    '''scalar test'''
+    spectra, theta_gw, phi_gw = spectra_theta_phi
+    SM = hsky.SkySensitivity(spectra, theta_gw, phi_gw, pol='vector-long')
+
+def test_scalar_trans_skymap(spectra_theta_phi):
+    '''scalar test'''
+    spectra, theta_gw, phi_gw = spectra_theta_phi
+    SM = hsky.SkySensitivity(spectra, theta_gw, phi_gw, pol='scalar-trans')
