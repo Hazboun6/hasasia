ec5e834a704bbfb5b6ba3d0623c9d09c455b4cd2
Hazboun6 <jeffrey.hazboun@gmail.com>
2020-06-10 14:21:04 -0700

    Fixes for docstrings

--- docs/conf.py
+++ docs/conf.py
@@ -100,6 +100,10 @@ html_theme = 'sphinx_rtd_theme'#'sphinxdoc'#'alabaster'
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
+html_css_files = [
+    'custom.css',
+]
+# html4_writer=True
 numpydoc_show_class_members = False
 # class_members_toctree = False
 # -- Options for HTMLHelp output ---------------------------------------
