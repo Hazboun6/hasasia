186b718d1754280c268706bc3bf671483d39fd9c
Jeremy Baier <baierj@oregonstate.edu>
2023-04-05 08:51:03 -0700

    initial commit

--- hasasia/sensitivity.py
+++ hasasia/sensitivity.py
@@ -1189,6 +1189,31 @@ def red_noise_powerlaw(A, freqs, gamma=None, alpha=None):
 
     return A**2*(freqs/fyr)**(-gamma)/(12*np.pi**2) * yr_sec**3
 
+def red_noise_realistic_curve(A, freqs, gamma=None, alpha=None):
+    r"""
+    jeremy
+    Add power law red noise to the prefit residual power spectral density.
+    As :math:`P=A^2(f/fyr)^{-\gamma}`
+
+    Parameters
+    ----------
+    A : float
+        Amplitude of red noise.
+
+    gamma : float
+        Spectral index of red noise powerlaw.
+
+    freqs : array
+        Frequencies at which to calculate the red noise power law.
+    """
+    if gamma is None and alpha is not None:
+        gamma = 3-2*alpha
+    elif ((gamma is None and alpha is None)
+          or (gamma is not None and alpha is not None)):
+        ValueError('Must specify one version of spectral index.')
+
+    return A**2*(freqs/fyr)**(-gamma)/(12*np.pi**2) * yr_sec**3
+
 def S_h(A, alpha, freqs):
     r"""
     Add power law red noise to the prefit residual power spectral density.
