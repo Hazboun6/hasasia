ab1fd016a93d36f47d261a112e64356fd028144f
Jeremy Baier <baierj@oregonstate.edu>
2025-01-27 22:01:13 -0800

    add real data test module

---
+++ tests/test_real_data.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""Tests for `hasasia` package."""
+
+import pytest
+import numpy as np
+
+import hasasia.sensitivity as hsen
+import hasasia.sim as hsim
+import hasasia.skymap as hsky
+import hasasia.forecast as hcast
+import hasasia.real_data as hrd
