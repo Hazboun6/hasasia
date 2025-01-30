bf59746b270c4a341903b2235c74328f08ebfc5c
Jeremy Baier <baierj@oregonstate.edu>
2025-01-29 16:57:54 -0800

    making a few changes to the real_data.py

--- hasasia/real_data.py
+++ hasasia/real_data.py
@@ -2,6 +2,7 @@
 from __future__ import print_function
 """Real data module."""
 import numpy as np
+import scipy.linalg as sl
 from astropy import units as u
 import jax.numpy as jnp
 import jax.scipy as jsc
@@ -11,6 +12,8 @@ from enterprise.pulsar import Pulsar as ePulsar
 
 fyr = 1/(365.25*24*3600)
 
+
+## could add a thinning here to make it faster.
 def corr_from_psd_chromatic(freqs, psd, toas, obs_freqs, chr_idx, fref=1400., fast=True):
      """
      Calculates the correlation matrix over a set of TOAs for a given power
@@ -128,31 +131,69 @@ def get_noise_basis(epsr, toas, nmodes=100):
     Fmat = create_fourier_design_matrix(t, nmodes)
     return Fmat * D[:, None]
 
-def make_corr(psr, noise):
+def white_noise_corr(psr,
+                    noise_dict,
+                    equad_convention='tnequad',
+                    include_ecorr=True):
+    """
+    Function to make white noise correlation matrix.
+    Formerly, `make_corr` in the real data tutorial.
+    Makes the white noise portion of the correlation matrix.
+    Stochastic signals can subsequently be added to this matrix.
+
+    Parameters
+    ----------
+    psr : enterprise.pulsar.Pulsar object
+        The pulsar object for which to create the correlation matrix.
+    noise_dict: dictionary
+        Dictionary containing noise parameters.
+    equad_convention : str, optional
+        Convention for equad parameter. Can be 'tnequad' or 't2equad'.
+    include_ecorr : bool, optional
+        Option to include ecorr in the white noise correlation matrix.
+        Should be turned on if the ecorr values are in the noise_dict.
+
+    Returns
+    -------
+    corr : array (Ntoa x Ntoa)
+        The white noise correlation matrix.
+    """
+    ### formally make_corr
     N = psr.toaerrs.size
     corr = np.zeros((N,N))
+    ### need to finagle some flags here
     _, _, fl, _, bi = hsen.quantize_fast(psr.toas,psr.toaerrs,
                                          flags=psr.flags['f'],dt=1)
-    keys = [ky for ky in noise.keys() if psr.name in ky]
+    keys = [ky for ky in noise_dict.keys() if psr.name in ky]
     backends = np.unique(psr.flags['f'])
     sigma_sqr = np.zeros(N)
     ecorrs = np.zeros_like(fl,dtype=float)
     for be in backends:
+        #### FIXME: check the flags
         mask = np.where(psr.flags['f']==be)
         key_ef = '{0}_{1}_{2}'.format(psr.name,be,'efac')
-        key_eq = '{0}_{1}_log10_{2}'.format(psr.name,be,'t2equad')
-        sigma_sqr[mask] = (
-                            noise[key_ef]**2 *
-                           ((psr.toaerrs[mask])**2 + (10**noise[key_eq])**2)
-                          )
-        mask_ec = np.where(fl==be)
-        key_ec = '{0}_{1}_log10_{2}'.format(psr.name,be,'ecorr')
-        ecorrs[mask_ec] = np.ones_like(mask_ec) * (10**noise[key_ec])
-    j = [ecorrs[ii]**2*np.ones((len(bucket),len(bucket)))
-         for ii, bucket in enumerate(bi)]
-
-    J = sl.block_diag(*j)
-    corr = np.diag(sigma_sqr) + J
+        key_eq = '{0}_{1}_log10_{2}'.format(psr.name,be,equad_convention)
+        if equad_convention == 'tnequad':
+            sigma_sqr[mask] = ( # variance_tn = efac^2 * toaerr^2 + equad^2
+                                noise_dict[key_ef]**2 *
+                            (psr.toaerrs[mask])**2 + (10**noise_dict[key_eq])**2
+                            )
+        elif equad_convention == 't2equad':
+            sigma_sqr[mask] = ( # variance_t2 = efac^2 * (toaerr^2 + equad^2)
+                                noise_dict[key_ef]**2 *
+                            ((psr.toaerrs[mask])**2 + (noise_dict[key_eq])**2)
+                            )
+        if include_ecorr:
+            mask_ec = np.where(fl==be)
+            key_ec = '{0}_{1}_log10_{2}'.format(psr.name,be,'ecorr')
+            ecorrs[mask_ec] = np.ones_like(mask_ec) * (10**noise_dict[key_ec])
+    if include_ecorr: # some PTAs don't have ecorr
+        j = [ecorrs[ii]**2*np.ones((len(bucket),len(bucket)))
+            for ii, bucket in enumerate(bi)]
+        J = sl.block_diag(*j)
+        corr = np.diag(sigma_sqr) + J
+    else:
+        corr = np.diag(sigma_sqr)
     return corr
 
 def corr_from_psd_dm(freqs, psd, toas, v_freqs, fast=True):
@@ -245,52 +286,51 @@ def hgw_calc(spectra, fyr):
     return hgw, plaw_h
 
 
-def noise_corr(ePsrs, noise, freqs, dm=False, chrom=False):
-    thin = 1
+def make_corr(ePsr, noise, freqs, dm=False, chrom=False, thin=1, A_gwb=1e-16, gamma_gwb=13/3.):
+    ePsr.toas = ePsr.toas[::thin]
+    ePsr.toaerrs = ePsr.toaerrs[::thin]
+    ePsr.Mmat = ePsr.Mmat[::thin,:]
     rn_psrs = get_red_noise(noise, 'red_noise')
     dm_n_psrs = get_red_noise(noise, 'dm_gp')
     chrom_n_psrs = get_red_noise(noise, 'chrom_gp')
-    psrs = []
 
-    for ePsr in ePsrs:
-        corr = make_corr(ePsr, noise)[::thin,::thin]
-        plaw = hsen.red_noise_powerlaw(A=1e-16, gamma=13/3., freqs=freqs)
-        corr += hsen.corr_from_psd(freqs=freqs, psd=plaw,
-                                toas=ePsr.toas[::thin])
-        # if rn:
-        if ePsr.name in rn_psrs.keys():
-            Amp, gam = rn_psrs[ePsr.name]
-            plaw_rn = hsen.red_noise_powerlaw(A=Amp, gamma=gam, freqs=freqs)
+    corr = white_noise_corr(ePsr, noise)[::thin,::thin]
+    plaw = hsen.red_noise_powerlaw(A_gwb=1e-16, gamma_gwb=13/3., freqs=freqs)
+    corr += hsen.corr_from_psd(freqs=freqs, psd=plaw,
+                            toas=ePsr.toas)
+    # if rn:
+    if ePsr.name in rn_psrs.keys():
+        Amp, gam = rn_psrs[ePsr.name]
+        plaw_rn = hsen.red_noise_powerlaw(A=Amp, gamma=gam, freqs=freqs)
 
-            corr += hsen.corr_from_psd(freqs=freqs, psd=plaw_rn,
-                                toas=ePsr.toas[::thin])
-        if dm:
-            if ePsr.name in dm_n_psrs.keys():
-                Amp, gam = dm_n_psrs[ePsr.name]
-                plaw_dm = hsen.red_noise_powerlaw(A=Amp, gamma=gam, freqs=freqs)
-                corr += hsen.corr_from_psd_dm(freqs=freqs, psd=plaw_dm,
-                                    toas=ePsr.toas[::thin], v_freqs=ePsr.freqs)
-        if chrom:
-            if ePsr.name in chrom_n_psrs.keys():
-                Amp, gam = chrom_n_psrs[ePsr.name]
-                key_chrom_idx = '{0}_chrom_gp_idx'.format(ePsr.name)
-                if key_chrom_idx in noise.keys():
-                    plaw_cn = hsen.red_noise_powerlaw(A=Amp, gamma=gam, freqs=freqs)
-                    corr += hsen.corr_from_psd_chrom(freqs=freqs, psd=plaw_cn,
-                                    toas=ePsr.toas[::thin], v_freqs=ePsr.freqs,
-                                      index=noise[key_chrom_idx])
-                else:
-                    plaw_cn = hsen.red_noise_powerlaw(A=Amp, gamma=gam, freqs=freqs)
-                    corr += hsen.corr_from_psd_chrom(freqs=freqs, psd=plaw_cn,
-                                    toas=ePsr.toas[::thin], v_freqs=ePsr.freqs,
-                                      index=4.)
-        psr = hsen.Pulsar(toas=ePsr.toas[::thin],
-                        toaerrs=ePsr.toaerrs[::thin],
-                        phi=ePsr.phi,theta=ePsr.theta,
-                        N=corr, designmatrix=ePsr.Mmat[::thin,:])
-        psr.name = ePsr.name
-        psrs.append(psr)
-    return psrs
+        corr += hsen.corr_from_psd(freqs=freqs, psd=plaw_rn,
+                            toas=ePsr.toas)
+    if dm:
+        if ePsr.name in dm_n_psrs.keys():
+            Amp, gam = dm_n_psrs[ePsr.name]
+            plaw_dm = hsen.red_noise_powerlaw(A=Amp, gamma=gam, freqs=freqs)
+            corr += hsen.corr_from_psd_dm(freqs=freqs, psd=plaw_dm,
+                                toas=ePsr.toas, v_freqs=ePsr.freqs)
+    if chrom:
+        if ePsr.name in chrom_n_psrs.keys():
+            Amp, gam = chrom_n_psrs[ePsr.name]
+            key_chrom_idx = '{0}_chrom_gp_idx'.format(ePsr.name)
+            if key_chrom_idx in noise.keys():
+                plaw_cn = hsen.red_noise_powerlaw(A=Amp, gamma=gam, freqs=freqs)
+                corr += hsen.corr_from_psd_chrom(freqs=freqs, psd=plaw_cn,
+                                toas=ePsr.toas, v_freqs=ePsr.freqs,
+                                    index=noise[key_chrom_idx])
+            else:
+                plaw_cn = hsen.red_noise_powerlaw(A=Amp, gamma=gam, freqs=freqs)
+                corr += hsen.corr_from_psd_chrom(freqs=freqs, psd=plaw_cn,
+                                toas=ePsr.toas, v_freqs=ePsr.freqs,
+                                    index=4.)
+    psr = hsen.Pulsar(toas=ePsr.toas,
+                      toaerrs=ePsr.toaerrs,
+                      phi=ePsr.phi,theta=ePsr.theta,
+                      N=corr, designmatrix=ePsr.Mmat)
+    psr.name = ePsr.name
+    return psr
 
 
 def calc_pta_gw(parpath, timpath, psrlist, noise, dm=False, chrom=False):
